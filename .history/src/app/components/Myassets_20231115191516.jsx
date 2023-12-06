f16cdf43dac7b174315c1d4cac03f26a3bf7ec27
ReturnerNaGithub <damsan1@gmail.com>
2023-12-06 16:53:42 +0100

    t commit

---
+++ .history/src/app/components/Myassets_20231115191516.jsx
@@ -0,0 +1,17 @@
+
+import Head from "next/head";
+import React from "react";
+
+export default function MyAssets() {
+  return (
+    <div>
+    <main>
+      <Head>
+        <title>MyAssets</title>
+      </Head>
+      <div className={styles.body}>
+       
+      </div></main>
+    </div>
+  );
+}
