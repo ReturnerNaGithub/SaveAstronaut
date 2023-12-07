fb06dcf24482974218c0fc17c6f8b97983f8e237
Ayan Maiti <mightyayan369@gmail.com>
2023-12-07 23:14:47 +0530

    Render NFT images

--- src/app/components/AstroSuitParts.jsx
+++ src/app/components/AstroSuitParts.jsx
@@ -6,15 +6,51 @@ import {
 } from "@web3modal/ethers/react";
 import { BrowserProvider, Contract, formatUnits } from "ethers";
 import { SourceMinter, AstroSuitPartsNFT } from "../../artifacts/artifacts";
+const imageUris = [
+  "https://gateway.pinata.cloud/ipfs/QmdTDsjaih5GmcZteJKsXk7nxhhrD9GAnvyUWzggvufzzu/gloves.png",
+  "https://gateway.pinata.cloud/ipfs/QmdTDsjaih5GmcZteJKsXk7nxhhrD9GAnvyUWzggvufzzu/helmet.png",
+  "https://gateway.pinata.cloud/ipfs/QmdTDsjaih5GmcZteJKsXk7nxhhrD9GAnvyUWzggvufzzu/suit.png",
+  "https://gateway.pinata.cloud/ipfs/QmdTDsjaih5GmcZteJKsXk7nxhhrD9GAnvyUWzggvufzzu/boots.png",
+];
 
 const AstroSuitParts = () => {
   const [astroSuitPartsNFTContract, setAstroSuitPartsNFTContract] =
     useState(null);
   const [balances, setBalances] = useState([]);
-  const [metadatas, setMetadatas] = useState([]);
+  // const [metadatasAndBalances, setMetadatas] = useState({});
 
   const { walletProvider } = useWeb3ModalProvider();
   const { isConnected, address } = useWeb3ModalAccount();
+
+  const getAstroSuitBalancesAndMetadata = async (address) => {
+    const metadataAndBalancesArray = [];
+    try {
+      console.log("hello");
+      const fetchedBalances = await astroSuitPartsNFTContract.balanceOfBatch(
+        [address, address, address, address],
+        [0, 1, 2, 3]
+      );
+      // setBalances((balances) => [...balances, fetchedBalances]);
+      console.log(fetchedBalances[0]);
+
+      for (let i = 0; i < 4; i++) {
+        const uri = await astroSuitPartsNFTContract.tokenURI(i);
+        const res = await (await fetch(uri)).json();
+        console.log((await res).image);
+        metadataAndBalancesArray.push({
+          balance: fetchedBalances[i],
+          image: (await res).image,
+        });
+      }
+
+      // console.log(metadataAndBalancesArray);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      console.log(metadataAndBalancesArray);
+      setBalances((balances) => [...metadataAndBalancesArray]);
+    }
+  };
 
   const getAstroSuitPartsNFTContract = async (walletProvider) => {
     try {
@@ -35,33 +71,6 @@ const AstroSuitParts = () => {
     }
   };
 
-  const getAstroSuitPartsTokenBalances = async (address) => {
-    try {
-      console.log("hello");
-      const uris = await astroSuitPartsNFTContract.balanceOfBatch(
-        [address, address, address, address],
-        [0, 1, 2, 3]
-      );
-      setBalances((balances) => [...balances, uris]);
-      console.log(uris[0]);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const getAstroSuitPartsTokenURIS = async (tokenId) => {
-    try {
-      //   console.log("hello");
-      const uri = await astroSuitPartsNFTContract.tokenURI(tokenId);
-
-      console.log(uri);
-      const response = await (await fetch(uri)).json();
-      console.log(await response.json());
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   useEffect(() => {
     if (walletProvider && isConnected) {
       getAstroSuitPartsNFTContract(walletProvider);
@@ -70,20 +79,28 @@ const AstroSuitParts = () => {
   }, []);
 
   useEffect(() => {
-    if (address && astroSuitPartsNFTContract) {
-      getAstroSuitPartsTokenBalances(address);
+    if (address && astroSuitPartsNFTContract && isConnected) {
+      getAstroSuitBalancesAndMetadata(address);
+      console.log(balances);
     }
   }, [astroSuitPartsNFTContract]);
 
-  useEffect(() => {
-    if (address && astroSuitPartsNFTContract && balances) {
-      balances.map(async (balance, idx) => {
-        return await getAstroSuitPartsTokenURIS(idx);
-      });
-    }
-  }, [balances]);
-
-  return <div>AstroSuitParts</div>;
+  return (
+    <div className="p-4 flex flex-row">
+      {balances.map(
+        (item, idx) =>
+          item.balance > 0 && (
+            <img
+              className="mx-4"
+              key={idx}
+              src={item.image}
+              alt=""
+              width="270px"
+            />
+          )
+      )}
+    </div>
+  );
 };
 
 export default AstroSuitParts;
